ab91e8406036a838f83f7efd967ce29280025eff
assarte <assarte.draven@gmail.com>
2015-02-17 01:19:07 +0100

    Minor HTML5 semantic bugfixes + added gitignore

--- public/index.php
+++ public/index.php
@@ -17,8 +17,10 @@ define('PERIDOT_DIR_LOCALROOT', realpath(PERIDOT_PATH_LOCALROOT));
 
 if (!file_exists(PERIDOT_DIR_LOCALROOT.'/'.PERIDOT_DIR_LIBS)) {
     header('HTTP/1.1 503 Service Unavailable');
+    echo '<html><head><style type="text/css">* { font-family: sans-serif; } code { font-family: monospace; background: #eee; padding: 3px; color: #666; }</style></head><body>';
     echo '<h1>Service Unavailable</h1>';
-    echo '<p>This copy of <a href="https://github.com/assarte/peridot">Peridot</a> looks uninitialized and need to be configured in <pre>composer.json</pre>, then to do a <pre>php composer.phar update</pre> on the project root in console.</p>';
+    echo '<p>This copy of <a href="https://github.com/assarte/peridot">Peridot</a> looks uninitialized and need to be configured in <code>composer.json</code>, then to do a <code>php composer.phar update</code> on the project root in console.</p>';
+    echo '</body></html>';
 }
 
 require PERIDOT_DIR_LOCALROOT.'/'.PERIDOT_DIR_LIBS.'/autoload.php';
@@ -26,6 +28,8 @@ require PERIDOT_DIR_LOCALROOT.'/'.PERIDOT_DIR_SHARED.'/bootstrap.php';
 
 if (defined('PERIDOT_EMPTY_BOOTSTRAP')) {
     header('HTTP/1.1 503 Service Unavailable');
+    echo '<html><head><style type="text/css">* { font-family: sans-serif; } code { font-family: monospace; background: #eee; padding: 3px; color: #666; }</style></head><body>';
     echo '<h1>Service Unavailable</h1>';
-    echo '<p>You may configured this copy of <a href="https://github.com/assarte/peridot">Peridot</a>, but it still looks weird because you didn\'t complete the <pre>bootstrap.php</pre> file found at <pre>~/'.PERIDOT_DIR_SHARED.'</pre>. Let\'s do it ASAP!</p>';
+    echo '<p>You may configured this copy of <a href="https://github.com/assarte/peridot">Peridot</a>, but it still looks weird because you didn\'t complete the <code>bootstrap.php</code> file found at <code>~/'.PERIDOT_DIR_SHARED.'</code>. Let\'s do it ASAP!</p>';
+    echo '</body></html>';
 }
